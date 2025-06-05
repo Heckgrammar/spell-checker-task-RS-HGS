4e8bfe9b1d16c550867d924bdd6bad1227ff3334
RS-HGS <144434850+RS-HGS@users.noreply.github.com>
2025-06-05 10:13:23 +0100

    Update Program.cs

--- SpellCheckerTask/Program.cs
+++ SpellCheckerTask/Program.cs
@@ -3,46 +3,58 @@ using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 using System;
-
 namespace SpellCheckerTask
+
 {
-    internal class Program
+    class Program
+
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // Read words from file - done up to 5 wors
+        
+        string[] All = File.ReadAllLines("WordsFile.txt");
+
+        string first = All[0].ToLower();
+        string second = All[1].ToLower();
+        string third = All[2].ToLower();
+        string fourth = All[3].ToLower();
+        string fifth = All[4].ToLower();
+        Console.Write("Enter one word: ");
+        string inputWord = Console.ReadLine().ToLower();
+
+        if (inputWord == first || inputWord == second || inputWord == third || inputWord == fourth || inputWord == fifth)
         {
-            string[] words = createDictionary();
-            //1. Take a user input of a word an check if it has been spelled correctly
-
-            //2. Take a string of words as a user input and check they have all been spelled correctly
-
-            //3.Create a spelling score based on the percentage of words spelled correctly
-
-            //4.Create a new list of words that have been spelled incorrectly and save it in a new file
-
-            //Challenge - Hard task
-
-            //Try to work out which words the user is trying to spell by looking for similarities in
-            //the spelling and ask the user did they mean that.
-
-            //Add these suggested words to a spelling list that the user can save as a file to work on
-            //their own spelling
-
-
-
+            Console.WriteLine("Correct spelling!");
         }
-        static string[] createDictionary()
+        else
         {
-            using StreamReader words = new("WordsFile.txt");
-            int count = 0;
-            string[] dictionaryData = new string[178636];
-            while (!words.EndOfStream)
+            Console.WriteLine("Incorrect spelling.");
+        }
+        
+
+        // Ask for a sentence
+        Console.Write("Please Enter a sentence: ");
+        string sentence = Console.ReadLine().ToLower();
+        string[] sentenceWords = sentence.Split(' ');
+
+        int totalWords = 0;
+        int correctWords = 0;
+
+        foreach (string word in sentenceWords)
+        {
+            totalWords++;
+
+            if (word == first || word == second || word == third || word == fourth || word == fifth)
             {
-
-                dictionaryData[count] = words.ReadLine();
-                count++;
+                correctWords++;
             }
-            words.Close();
-            return dictionaryData;
         }
+
+        // Display the score thry got for the test
+        double score = (double)correctWords / totalWords * 100;
+        Console.WriteLine("\nCorrect words: " + correctWords + "/" + totalWords);
+        Console.WriteLine("Spelling Score: " + score + "%");
     }
+}
 }
